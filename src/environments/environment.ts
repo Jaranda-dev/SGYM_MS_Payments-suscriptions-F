8db79b0764143f47614171185524733ef0a176c9
Jessar-Dev <jesus_aranda_rdz@hotmail.com>
2025-08-18 12:58:12 -0600

    sdsd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment =  {
   production: true,
   apiUrl: 'http://143.110.150.81/api/pagos',
   apiAuth: 'http://143.110.150.81',
-  apiNG: 'http://143.110.150.81/api/ng',
+  apiNG: 'http://143.110.150.81/api/negocio',
   url : 'http://146.190.130.50',
   STRIPE_PUBLIC_KEY: 'pk_test_51RsQ3s1ArB5UTM0zaB04JTb5FuDKhkrSbni48JJ6cnZK8JGDUbfx6FftuL6tIdYlGXXK4SDNGqkYRIyhfA851qA400yoaE6nNh'
 };
